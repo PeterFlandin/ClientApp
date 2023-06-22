b7c546e037f399c991f3e6cce3b836376dd9c347
PeterFlandin <peterflandin@gmail.com>
2023-06-22 16:42:38 +0200

    first commit

---
+++ src/test/java/edu/datascientest/clientapp/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.datascientest.clientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
